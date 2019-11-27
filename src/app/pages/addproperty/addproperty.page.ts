6e322b24c1bd93d49f15bb5909bddf4fb137bc40
Gaopalelwe10 <gmpetshane@gmail.com>
2019-11-27 08:03:43 +0200

    Appproduct second commit

--- src/app/pages/addproperty/addproperty.page.ts
+++ src/app/pages/addproperty/addproperty.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-addproperty',
@@ -6,8 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./addproperty.page.scss'],
 })
 export class AddpropertyPage implements OnInit {
+  AddpropertyForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+  ) {
+    this.AddpropertyForm = fb.group({
+      description: ['', Validators.required],
+      price: ['', Validators.required],
+      location: ['', Validators.required],
+      typeofproperty: ['', Validators.required],
+      bedrooms: ['', Validators.required],
+      bathrooms: ['', Validators.required],
+      kitchens: ['', Validators.required],
+      garage: ['', Validators.required],
+      lounges: ['', Validators.required],
+      garden: ['', Validators.required],
+      pets: ['', Validators.required],
+      pool: ['', Validators.required],
+      diningroom :['', Validators.required],
+    });
+  }
 
   ngOnInit() {
   }
