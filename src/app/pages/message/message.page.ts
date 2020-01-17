a6c47a5e350cb77fdf82ba5ae4143771e3bcc7ff
gontsejnr <gontsemaepa@gmail.com>
2020-01-17 12:06:14 +0200

    message

--- src/app/pages/message/message.page.ts
+++ src/app/pages/message/message.page.ts
@@ -10,15 +10,15 @@ export class MessagePage implements OnInit {
 
   constructor(private contacts: Contacts) { 
 
-    let contact: Contact = this.contacts.create();
+  //   let contact: Contact = this.contacts.create();
 
-    contact.name = new ContactName (null, '', '');
-    contact.phoneNumbers = [new ContactField('mobile', '')];
-    contact.save().then(
-      () => console.log('Contact saved!', contact),
-      (error: any) => console.error('Error saving contact.', error)
-    );
-  }
+  //   contact.name = new ContactName (null, '', '');
+  //   contact.phoneNumbers = [new ContactField('mobile', '')];
+  //   contact.save().then(
+  //     () => console.log('Contact saved!', contact),
+  //     (error: any) => console.error('Error saving contact.', error)
+  //   );
+   }
 
   ngOnInit() {
   }
