1e26b858c4e083cfdd3794753e9b597dcb9ed1e5
CodyNetsh <khodani1994@gmail.com>
2020-01-24 11:53:54 +0200

    message

--- src/app/pages/message/message.page.ts
+++ src/app/pages/message/message.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Contacts, Contact, ContactField, ContactName} from '@ionic-native/contacts/ngx';
+import { MessageService } from 'src/app/services/message.service';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-message',
@@ -8,12 +10,19 @@ import { Contacts, Contact, ContactField, ContactName} from '@ionic-native/conta
 })
 export class MessagePage implements OnInit {
 
+  messegeList: any;
+ 
+
   segmentChanged(ev: any) {
     console.log('Segment changed', ev);
   }
 
-  constructor(private contacts: Contacts) { 
+  constructor(private contacts: Contacts,
+    private messageService: MessageService,
+    private afAuth: AngularFireAuth,
+    ) { 
 
+ 
   //   let contact: Contact = this.contacts.create();
 
   //   contact.name = new ContactName (null, '', '');
@@ -23,6 +32,7 @@ export class MessagePage implements OnInit {
   //     (error: any) => console.error('Error saving contact.', error)
   //   );
    }
+
 
   ngOnInit() {
   }
