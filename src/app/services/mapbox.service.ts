b64791d31108eea73b67b87660a0392425956d21
Gaopalelwe-Mpetshane <gmpetshane@gmail.com>
2020-01-24 12:12:37 +0200

    small changes

--- src/app/services/mapbox.service.ts
+++ src/app/services/mapbox.service.ts
@@ -27,7 +27,6 @@ export class MapboxService {
   constructor(private http: HttpClient, private dog: AngularFirestore) { }
 
   search_word(query: string) {
-
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/';
     return this.http.get(url + query + '.json?types=address&country=ZA&access_token=pk.eyJ1IjoibmVvLXB1bGUiLCJhIjoiY2p4cTI0MGF0MGlnajNjbDMzMW9nMzJ6OSJ9.QgND5rJKyVYEmTjBJIrq3g')
       .pipe(map((res: MapboxOutput) => {
